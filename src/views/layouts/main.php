fef148cd9afef579035e1cca2c2be551838b42da
ahmet <ahmet@vademecumonline.com.tr>
2022-06-08 15:30:27 +0300

    fixed page title

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -23,7 +23,6 @@ foreach ($notificationTypes as $notificationType) {
     <head>
         <meta charset="<?= Yii::$app->charset ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title><?= Yii::$app->params['projectName'] ?> | Log in</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php $this->registerCsrfMetaTags() ?>
